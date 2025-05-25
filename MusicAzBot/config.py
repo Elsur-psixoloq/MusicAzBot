461247fa0077e00e07a4b4d28df3ecd0f112836b
Elsur-psixoloq <Tr.arabova@gmail.com>
2025-05-25 21:27:28 +0400

    Update config.py

--- MusicAzBot/config.py
+++ MusicAzBot/config.py
@@ -3,13 +3,13 @@ import os
 
 class Config:
 
-   API_ID = int(os.getenv("API_ID", "8953338"))
-   API_HASH = os.getenv("API_HASH", "fe21f223cb02d8f7c1cbda651f553a45")
-   BOT_TOKEN = os.getenv("BOT_TOKEN", "2142897671:ghyujjhgggggggggggggfddddddddddddtttttttttttttt")
-   BOT_USERNAME = os.environ.get("BOT_USERNAME", "MusicAzBot")
-   OWNER_NAME = os.environ.get("OWNER_NAME", "HuseynH") 
-   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "MusicAzPlaylist")
-   PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001692410500"))
-   ALIVE_NAME = os.environ.get("ALIVE_NAME", "HUSEYN")
+   API_ID = int(os.getenv("API_ID", "19769686"))
+   API_HASH = os.getenv("API_HASH", "515b64f5d2d955cdd6aa85a808fd4cb4")
+   BOT_TOKEN = os.getenv("BOT_TOKEN", "7340381347:AAFfVgtVNSyFCX-XNJav0gDGbRdXUODDRXA")
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "XezerMusicRobot")
+   OWNER_NAME = os.environ.get("OWNER_NAME", "Lerrodu") 
+   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "LerroBlogs")
+   PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1002529622504"))
+   ALIVE_NAME = os.environ.get("ALIVE_NAME", "LERRO")
    ALIVE_IMG = os.environ.get("ALIVE_IMG", "https://telegra.ph/file/f6c186e3c581a223856c4.mp4") 
    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/dc9794139c12507f5eb1c.jpg")    
